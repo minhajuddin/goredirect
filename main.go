f9faf95b75ec90d3e41e17a287a09f2e6b7437e1
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2012-12-18 02:08:18 +0530

    Got it working with redis

--- main.go
+++ main.go
@@ -5,32 +5,41 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"github.com/alphazero/Go-Redis"
+	"errors"
 )
 
 var config map[string]string
+var client redis.Client
 
 func main(){
 	port := os.Getenv("PORT")
 	log.Println("loading cofing")
-	loadConfig()
+	spec := redis.DefaultSpec()
+	client,_ = redis.NewSynchClientWithSpec(spec);
 	log.Printf("Started redirector at %v\n", port)
 	http.ListenAndServe(":"+port, &redirector{})
 }
 
-func loadConfig() error{
-	config = map[string]string{"localhost:3000" : "http://cosmicvent.com", "ml:3000" : "http://minhajuddin.com",}
-	return nil
+func getHost(host string) (string, error){
+	var err error
+	lbytes, err := client.Get(host)
+	if err == nil &&  len(lbytes) == 0 {
+		err = errors.New("Location not found")
+	}
+	return string(lbytes), err
 }
 
 type redirector struct{}
 func (self *redirector) ServeHTTP(w http.ResponseWriter, r *http.Request){
 	host := r.Host
-	location, ok := config[host]
-	if !ok {
-		err := fmt.Sprintf("Did not find config for '%s'\n", host)
-		log.Print(err)
+	location, err := getHost(host)
+	if err != nil {
+		log.Println(err)
+		message := fmt.Sprintf("Did not find config for '%s'\n", host)
+		log.Println(message)
 		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte(err))
+		w.Write([]byte(message))
 		return
 	}
 
