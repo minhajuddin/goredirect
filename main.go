3c46ca96a6c3f30e104a1b20dce709a1e98809bb
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2012-12-18 02:11:38 +0530

    Working version of redirector

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -10,24 +11,29 @@ var config map[string]string
 
 func main(){
 	port := os.Getenv("PORT")
+	log.Println("loading cofing")
+	loadConfig()
 	log.Printf("Started redirector at %v\n", port)
 	http.ListenAndServe(":"+port, &redirector{})
 }
 
 func loadConfig() error{
-	config = map[string]string{"localhost" : "cosmicvent.com", "ml" : "minhajuddin.com",}
+	config = map[string]string{"localhost:3000" : "http://cosmicvent.com", "ml:3000" : "http://minhajuddin.com",}
 	return nil
 }
 
 type redirector struct{}
 func (self *redirector) ServeHTTP(w http.ResponseWriter, r *http.Request){
-	location, ok := config[r.URL.Host]
+	host := r.Host
+	location, ok := config[host]
 	if !ok {
-		log.Printf("Did not find config for %s\n", location)
+		err := fmt.Sprintf("Did not find config for '%s'\n", host)
+		log.Print(err)
 		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("Did not find config for " + location))
+		w.Write([]byte(err))
 		return
 	}
-	w.WriteHeader(http.StatusMovedPermanently)
-	w.Write([]byte("please go to <a href='http//:" + location + ">" + location + "</a>"))
+
+	http.Redirect(w, r, location, http.StatusMovedPermanently)
+	//w.Write([]byte("please go to <a href='http//:" + location + ">" + location + "</a>"))
 }
